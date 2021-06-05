f8e0f1dca2ec7baab43bb27e43376d58b503a2f5
PhKhang <trannguyenphuckhang12@gmail.com>
2021-06-05 14:35:55 +0700

    last change

--- muscle.js
+++ muscle.js
@@ -1,1 +1,9 @@
-prompt("yo!")
+window.onload = function() {
+
+
+var doantho = document.getElementsByClassName("tho")[0];
+
+//doantho.addEventListener("click", foo);
+function foo(){
+    doantho.style.background= "red";    
+}}
