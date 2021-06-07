30facbb5f53f6baa6de124a817a64478e33c0034
PhKhang <trannguyenphuckhang12@gmail.com>
2021-06-07 16:21:38 +0700

    big error fixed

--- muscle.js
+++ muscle.js
@@ -6,34 +6,40 @@ var lastday = function (y, m) {
 }
 
 window.onload = function () {
-    document.getElementById("thang").innerHTML = "Tháng " + dt.getMonth() + ', ' + dt.getFullYear();
-    chinh();
     thangmoi = dt.getMonth();
     nammoi = dt.getFullYear();
+    document.getElementById("thang").innerHTML = "Tháng " + (thangmoi + 1) + ', ' + nammoi;
+    chinh();
+    
 
 }
 
 function thangtruoc() {
     thangmoi--;
-    if (thangmoi < 1) { thangmoi = 12; dt = new Date(new Date().getFullYear(), thangmoi, nammoi--) }
+    if (thangmoi < 0) { thangmoi = 11; dt = new Date(new Date().getFullYear(), thangmoi, nammoi--) }
     else dt = new Date(new Date().getFullYear(), thangmoi, new Date().getDate());
     chinh();
-    document.getElementById("thang").innerHTML = "Tháng " + thangmoi + ', ' + nammoi;
+    document.getElementById("thang").innerHTML = "Tháng " + (thangmoi + 1) + ', ' + nammoi;
 }
 function thangsau() {
     thangmoi++;
-    if (thangmoi > 12) { thangmoi = 1; dt = new Date(new Date().getFullYear(), thangmoi, nammoi++) }
+    if (thangmoi > 11) { thangmoi = 0; dt = new Date(new Date().getFullYear(), thangmoi, nammoi++) }
     else dt = new Date(new Date().getFullYear(), thangmoi, new Date().getDate());
     chinh();
-    document.getElementById("thang").innerHTML = "Tháng " + thangmoi + ', ' + nammoi;
+    document.getElementById("thang").innerHTML = "Tháng " + (thangmoi + 1) + ', ' + nammoi;
 }
 function chinh() {
     var week = ["week1", "week2", "week3", "week4", "week5", "week6"];
     var iOfWeek = 0;
     var days = ["thu8", "thu2", "thu3", "thu4", "thu5", "thu6", "thu7"];
+    for (var a=0; a<6; a++){
+        for (var b=0; b<7; b++){
+            document.getElementsByClassName(week[a])[0].getElementsByClassName(days[b])[0].innerHTML = '';
+        }
+    }
     for (var i = 1; i <= lastday(dt.getFullYear(), dt.getMonth()); i++) {
         var ngay = new Date(dt.getFullYear(), dt.getMonth(), i);
-        console.log(days[ngay.getDay()] + " ");
+        console.log(ngay.getDate() + " ");
         document.getElementsByClassName(week[iOfWeek])[0].getElementsByClassName(days[ngay.getDay()])[0].innerHTML = i;
         document.getElementsByClassName(week[iOfWeek])[0].getElementsByClassName(days[ngay.getDay()])[0].style = "border: 0px;";
 
@@ -41,6 +47,7 @@ function chinh() {
             if (dt.getDay() != 0) { document.getElementsByClassName(week[iOfWeek])[0].getElementsByClassName(days[ngay.getDay()])[0].style.cssText = "box-sizing: border-box; border: 2px solid var(--foregroundColor); box-shadow: inset -5px -5px 6px rgba(255, 255, 255, 0.5), inset 5px 5px 6px rgba(0, 0, 0, 0.33),-5px -5px 10px rgba(255, 255, 255, 0.5), 5px 5px 10px rgba(0, 0, 0, 0.33); background-color: hsl(221, 75%, 78%)"; }
             else document.getElementsByClassName(week[iOfWeek])[0].getElementsByClassName(days[ngay.getDay()])[0].style.cssText = "box-sizing: border-box; border: 2px solid hsl(3, 100%, 87%); box-shadow: inset -5px -5px 6px rgba(255, 255, 255, 0.5), inset 5px 5px 6px rgba(0, 0, 0, 0.33),-5px -5px 10px rgba(255, 255, 255, 0.5), 5px 5px 10px rgba(0, 0, 0, 0.33); background-color: hsl(3, 100%, 78%)";
         }
+        document.getElementsByClassName(week[iOfWeek])[0].style.visibility = "visible";
         if (ngay.getDay() == 0 && ngay.getDate() != lastday(dt.getFullYear(), dt.getMonth())) { iOfWeek++; }
     }
     for (iOfWeek++; iOfWeek < 6; iOfWeek++) {
